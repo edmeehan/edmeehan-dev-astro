1a6623a1ba237fdd8b59dbcb0e39f5650b9f9e16
Edward Meehan <edward.meehan.808@gmail.com>
2023-09-12 17:39:29 -0700

    added react & mdx to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import bookshop from '@bookshop/astro-bookshop';
+import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [bookshop()],
-  trailingSlash: 'always',
+  integrations: [bookshop(), react(), mdx()],
+  trailingSlash: 'always'
 });
