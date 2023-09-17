879a72cd641d177ab03f8058cf25032c50cd3708
edmeehan <edward.meehan.808@gmail.com>
2023-09-17 02:13:41 -0700

    1 file modified

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,10 +1,13 @@
 import { sequence } from 'astro:middleware';
 
-async function imgix(_, next) {
-  const response = await next();
-  const html = await response.text();
-  console.log('validation response', html);
-  return response;
-}
+// async function imgix(_, next) {
+//   const response = await next();
+//   const html = await response.text();
+//   console.log('validation response', html);
+//   return new Response(html, {
+//     status: 200,
+//     headers: response.headers,
+//   });
+// }
 
-export const onRequest = sequence(imgix);
+// export const onRequest = sequence(imgix);
